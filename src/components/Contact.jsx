ac41e2a6a59a2d12ee960bf20eb61efb159a94c5
Zurabi Kochiashvili <78660887+Zurab1K@users.noreply.github.com>
2025-08-22 00:26:25 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="text-4xl md:text-5xl font-semibold mb-10"
+          className="text-4xl md:text-5xl font-semibold mb-16"
         >
           Let’s Connect
         </motion.h2>
