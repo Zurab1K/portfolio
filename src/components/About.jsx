fb1773ad953feb1d0dfd874b958e60a04d753131
Zurabi Kochiashvili <78660887+Zurab1K@users.noreply.github.com>
2025-08-22 00:24:49 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="text-4xl md:text-5xl font-semibold mb-8"
+          className="text-4xl md:text-5xl font-semibold mb-16"
         >
           About Me
         </motion.h2>
